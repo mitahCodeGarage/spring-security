a38cbe9ed94cd8e3f174bf0e80199ac504220ad6
mitahCodeGarage <motorwalahatim@gmail.com>
2024-11-21 07:35:52 +0530

    Add files via upload

---
+++ src/main/java/com/mitahcodegarage/poc/spring_security/config/authentication/UserDetailsServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mitahcodegarage.poc.spring_security.config.authentication;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails userDetails = new UserDetailsImpl(username, "password");
+        return userDetails;
+    }
+}
